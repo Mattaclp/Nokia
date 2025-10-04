7c419a7674b74cf75fe385eb03e5f58eda6649da
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2025-10-04 12:12:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,5 @@
 sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+rm -rf feeds/packages/devel/gn/Makefile
+wget https://github.com/Mattaclp/NewLEDE/raw/refs/heads/main/gn/gnMakefile
+mv gnMakefile feeds/packages/devel/gn/Makefile
