6e409f5b572be98fad2745f5eab25a21f9e3d34d
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2025-12-15 20:40:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
-git reset --hard b16b75efbac2900ea48fc3302a0e28b80e6b70a6
+#git reset --hard b16b75efbac2900ea48fc3302a0e28b80e6b70a6
 
 # add passwall
 #sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
